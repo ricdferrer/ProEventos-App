8f983cca6720757f3c53ede7ca68e1129cccdd02
Ricardo <ricardodferrer@gmail.com>
2022-09-05 00:44:30 -0300

    grid completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -4,23 +4,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  exibirImagem: boolean = false;
+  private _filtroLista: string = '';
 
-  public eventos: any;
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
 
-  constructor(private https: HttpClient) { }
+  filtrarEventos(filtrarPor: string) {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(private https: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
 
   public getEventos(): void {
-    this.https.get("https://localhost:5001/api/eventos").subscribe(
-      response => this.eventos = response,
-      error => console.error(error)
-    );
+    this.https.get('https://localhost:5001/api/eventos').subscribe({
+      next: (r) => {
+        this.eventos = r;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (e) => console.error(e),
+    });
   }
-
 }
